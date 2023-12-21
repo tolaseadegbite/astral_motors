9eafb81dbb3ab0a908d9750d35d82ddc5c7b4746
tolaseadegbite <tolasekelvin@gmail.com>
2023-12-21 02:07:18 +0100

    setup testing

--- Gemfile
+++ Gemfile
@@ -51,7 +51,7 @@ group :test do
   gem "selenium-webdriver"
   gem "webdrivers"
   gem "rails-controller-testing", "1.0.5"
-  gem "minitest",                 "5.15.0"
+  gem "minitest",                 "5.20.0"
   gem "minitest-reporters",       "1.5.0"
   gem "guard",                    "2.18.0"
   gem "guard-minitest",           "2.4.6"
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
     protected
 
     def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :surname, :address, :phone, :avatar, :next_of_kin, :next_of_kin_phone, :date_of_birth, :admin])
+        devise_parameter_sanitizer.permit(:sign_up, keys: k[:first_name, :surname, :address, :phone, :avatar, :next_of_kin, :next_of_kin_phone, :date_of_birth, :admin])
         devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :surname, :address, :phone, :avatar, :next_of_kin, :next_of_kin_phone, :date_of_birth, :admin])
     end
 end
