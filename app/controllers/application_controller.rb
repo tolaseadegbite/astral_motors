65b9bbf54ff9e954941e0d4651e48f5281a8d4f6
tolaseadegbite <tolasekelvin@gmail.com>
2023-12-21 01:57:39 +0100

    Setup devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :surname, :address, :phone, :avatar, :next_of_kin, :next_of_kin_phone, :date_of_birth, :admin])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :surname, :address, :phone, :avatar, :next_of_kin, :next_of_kin_phone, :date_of_birth, :admin])
+    end
 end
---
+++ app/models/user.rb
@@ -0,0 +1,32 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :bigint           not null, primary key
+#  address                :string           not null
+#  admin                  :boolean          default(FALSE)
+#  date_of_birth          :date             not null
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  first_name             :string           not null
+#  next_of_kin            :string           not null
+#  next_of_kin_phone      :string           not null
+#  phone                  :string           not null
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string
+#  surname                :string           not null
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
